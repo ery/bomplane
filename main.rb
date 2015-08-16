5386fa8c3d8ab6b2bd19d36f53d16f173cb45ef4
Ery <erylei@gmail.com>
2015-08-16 12:59:25 +0800

    No Message.

--- main.rb
+++ main.rb
@@ -259,3 +259,36 @@ print "   |                                       \r\n"
 print "10 |                                       \r\n"
 print "   |                                       \r\n"
 
+
+new_page 1
+
+print "   | A   B   C   E   F   G   H   I   J   K  \r\n"
+print "---|----------------------------------------\r\n" 
+print " 1 |                                       \r\n"
+print "   |                                       \r\n"
+print " 2 |             X                         \r\n"
+print "   |                                       \r\n"
+print " 3 |     X   X   X   X   X                 \r\n"
+print "   |                                       \r\n"
+print " 4 |             X                         \r\n"
+print "   |                                       \r\n"
+print " 5 |         =  |||  =                     \r\n"
+print "   |                                       \r\n"
+print " 6 |                                       \r\n"
+print "   |                                       \r\n"
+print " 7 |                                       \r\n"
+print "   |                                       \r\n"
+print " 8 |                                       \r\n"
+print "   |                                       \r\n"
+print " 9 |                                       \r\n"
+print "   |                                       \r\n"
+print "10 |                                       \r\n"
+print "   |                                       \r\n"
+
+new_page 1
+
+puts "炸飞机"
+puts "开始游戏"
+
+
+
